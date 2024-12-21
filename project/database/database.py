3c53b12a9d12f2980e3a05450df7f8b3b2a2ad25
ravi <sankoju.ravi@machint.com>
2024-12-21 17:07:43 +0530

    bug fixes

--- project/database/database.py
+++ project/database/database.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.declarative import declarative_base
 #load_dotenv()
 Base = declarative_base()
 
-database_url = os.environ.get("DATABASE_URL","mysql+pymysql://remit_admin:remit_admin@127.0.0.1/tfs_python")
+database_url = os.environ.get("DATABASE_URL","mysql+pymysql://ThoshikaFS:Thoshika@FS456@127.0.0.1/tfs_python")
 engine = create_engine(database_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
